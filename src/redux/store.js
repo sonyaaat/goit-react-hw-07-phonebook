e0a3ebd2b18e5b4a796049b0150c72758a6f5cd5
sonyaaat <sofiatkach2004@gmail.com>
2022-12-14 15:34:15 +0200

    Fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from '../redux/contactsSlice';
 import { filterReducer } from './filterSlice';
 // import {
